dd09d663b1e525fa4232cb572ed14bbbffd07399
brett ripley <ripley.brett@gmail.com>
2016-11-10 16:04:49 -0500

    Add layout, home, and statecapitals bare bones components. Add Routes for each.

---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+const React = require('react')
+
+const Home = () => (
+  <h1>Dick Sauce!!!!!</h1>
+)
+
+module.exports = Home
---
+++ src/components/StateCapitals.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+
+const StateCapitals = React.createClass({
+  render () {
+    return (
+      <h1>Shitting Fire!!!</h1>
+    )
+  }
+})
+
+module.exports = StateCapitals
